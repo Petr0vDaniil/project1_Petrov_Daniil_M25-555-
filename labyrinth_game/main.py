9f65d6c3f98b4356f83fbf5985254433649082e5
Petrov Daniil <petr.dan.02@yandex.ru>
2026-01-12 09:12:44 +0300

    feat: создание архитектуры проекта и определение ROOMS с 8 комнатами

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+from labyrinth_game.constants import ROOMS
+
 def main():
-    print("Первая попытка запустить проект!")
+    game_state = {
+        'player_inventory': [],
+        'current_room': 'entrance',
+        'game_over': False,
+        'steps_taken': 0
+    }
 
 
 if __name__ == "__main__":
     main()
+
